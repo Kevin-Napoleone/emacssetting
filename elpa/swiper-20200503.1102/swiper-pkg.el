acd26e0b58c9b08d1d20985c2247d2a618221d64
Kevin-Napoleone <280643990@qq.com>
2020-06-30 01:04:49 +0800

    first commit

---
+++ elpa/swiper-20200503.1102/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "swiper" "20200503.1102" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.0")) :commit "77748673d3481f9fd1de91283c57ce535404c28f" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
